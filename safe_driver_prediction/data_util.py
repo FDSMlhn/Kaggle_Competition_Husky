da2d795555ba833f39b1390bef0cc044d8c96c82
FDSM_lhn <FDSM_lhn@yahoo.com>
2017-10-05 09:36:49 -0700

    start Modelling

---
+++ safe_driver_prediction/data_util.py
@@ -0,0 +1,22 @@
+import numpy as py
+import pickle
+import os
+import pandas as pd
+    
+    
+def load_test_data():
+    data_list = [] 
+    for i in range(5):
+        file = "datasets/test_batch_{}".format(i)
+        with open(file,'rb') as f:
+            data_list.append(pickle.load(file = f))
+    return pd.concat(data_list)
+
+
+def load_train_data():
+    data_list = [] 
+    for i in range(3):
+        file = "datasets/train_batch_{}".format(i)
+        with open(file,'rb') as f:
+            data_list.append(pickle.load(file = f))
+    return pd.concat(data_list)
