6384ef4b9259d4e2bf66565fe83da7acdf265a29
nannnth <fdsm_lhn@yahoo.com>
2017-11-18 17:21:08 +0000

    One XGBoost and lgb stacker

--- safe_driver_prediction/data_util.py
+++ safe_driver_prediction/data_util.py
@@ -1,7 +1,10 @@
 import pickle
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
-    
+from numba import jit
+
+
 def load_test_data():
     data_list = [] 
     for i in range(5):
@@ -29,4 +32,23 @@ def abandon_col(data,rso=1):
     data.drop(['id','target'], axis=1, inplace=True)
     return data, y
 
+@jit
+def eval_gini(y_true, y_prob):
+    y_true = np.asarray(y_true)
+    y_true = y_true[np.argsort(y_prob)]
+    ntrue = 0
+    gini = 0
+    delta = 0
+    n = len(y_true)
+    for i in range(n-1, -1, -1):
+        y_i = y_true[i]
+        ntrue += y_i
+        gini += y_i * delta
+        delta += 1 - y_i
+    gini = 1 - 2 * gini / (ntrue * (n - ntrue))
+    return gini
 
+def eval_gini_xgb(preds, dtrain):
+    labels = dtrain.get_label()
+    gini_score = -eval_gini(labels, preds)
+    return [('gini', gini_score)]
--- safe_driver_prediction/lgb_stacker.py
+++ safe_driver_prediction/lgb_stacker.py
@@ -1,80 +1,71 @@
 import pandas as pd
 import numpy as np
+import data_util
 
 import lightgbm as lgb
+from lightgbm import LGBMClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-
-lgb_train = lgb.Dataset(X_train, y_train, free_raw_data=False)
-lgb_val = lgb.Dataset(X_val, y_val, reference=lgb_train, free_raw_data=False)
+from sklearn import metrics
 
 lgb1_params = {
     'boosting_type':'gbdt',
     'objective':'binary',
-    'metric': {'l2','binary_logloss'},
+    'metric': {'binary_logloss'},
     'learning_rate': 0.05, 
     'feature_fraction': 0.9,   #
     'bagging_fraction': 0.8,   #
     'bagging_freq': 5,
-    'verbose': 0
+    'verbose': 0,
+    'lambda_l2':0.0342
 }
 
 lgb1_hyper = {'params': lgb1_params,
-             'train_set': None
-              'valid_sets': None
+             'train_set': None,
+              'valid_sets': None,
               'num_boost_round': 1500, 
-              'early_stopping_rounds': 5
+              'early_stopping_rounds': 5,
+                  'verbose_eval':50
              }
 
-# lgb1 = lgb.train(lgb1_params,
-#         lgb_train,
-#         num_boost_round =1500,  #Number of trees it will generate  
-#         valid_sets= lgb_val,
-#     early_stopping_rounds = 5
-#         )
 
 lgb2_params = {
     'boosting_type':'gbdt',
     'objective':'binary',
-    'metric': {'l2','binary_logloss'}, 
+    'metric': {'binary_logloss'}, 
     'feature_fraction': 0.8,
     'bagging_fraction': 0.9,
     'bagging_freq': 5,
-    'feature_freq' :10,
-    'num_leaves':24 
+    'num_leaves':24,
+    'lambda_l2':0.0342
 }
 
 lgb2_hyper = {'params': lgb2_params,
-             'train_set': None
-              'valid_sets': None
+             'train_set': None,
+              'valid_sets': None,
               'num_boost_round': 2500, 
               'learning_rates': lambda x: 0.05*(0.999**x), 
               'early_stopping_rounds': 5
              }
 
-# lgb2 = lgb.train(lgb2_params,
-#         lgb_train,
-#         valid_sets= lgb_val,
-#     num_boost_round = 2500,
-#     learning_rates= lambda x: 0.05*(0.999**x), #step_decay
-#     early_stopping_rounds = 5
-#         )
 
 lgb3_params = {
     'boosting_type':'gbdt', 
     'objective':'binary',
-    'metric': {'l2','binary_logloss'},
+    'metric': {'binary_logloss'},
     'learning_rate': 0.03, 
     'feature_fraction': 0.8, 
     'bagging_fraction': 0.8, 
     'bagging_freq': 10, 
-    'max_depth':6 
+    'max_depth':6,
+    'lambda_l2':0.0342
 }
 
 lgb3_hyper = {'params': lgb3_params,
-             'train_set': None
-              'valid_sets': None
+             'train_set': None,
+              'valid_sets': None,
               'num_boost_round': 2000, 
               'early_stopping_rounds': 10
              }
@@ -96,6 +87,7 @@ lgb3_hyper = {'params': lgb3_params,
 stack_model = LogisticRegression()
 all_base_models = {'lgb': [lgb1_hyper,lgb2_hyper,lgb3_hyper]}
 
+
 class lgb_ensemble():
     def __init__(self, n_splits, num_base_models, stacker= stack_model, base_models= all_base_models):
         self.n_splits = n_splits
@@ -103,40 +95,93 @@ class lgb_ensemble():
         self.stacker= stacker
         self.base_models= base_models
         
-    def fit_predict(self, X, y, T, cv = True):
-        Final_score_test = np.zeros((T.shape[0], self.num_base_models))
-        val_score_test = np.zeros((X.shape[0], self.num_base_models))
+    def fit_predict(self, X, y, T, META = None, rso=66):
+        miscellany= {}
+        
+        S_train = np.zeros((X.shape[0], self.num_base_models))
+        S_test  = np.zeros((T.shape[0], self.num_base_models))
         i = 0
-
+        
+        folds = list(StratifiedKFold(n_splits=self.n_splits, shuffle=True, random_state=rso).split(X, y))
+        
         for lgb_hyper in self.base_models.get('lgb'):
-            folds = list(StratifiedKFold(n_splits=self.n_splits, shuffle=True, random_state=2016).split(X, y))
+            S_test_i =  np.zeros((T.shape[0], self.n_splits))
+            
             for j, (train_idx, val_idx) in enumerate(folds) :
-                lgb_hyper['train_set'] = lgb.Dataset(X.iloc[train_idx, :], 
-                                            y.iloc[train_idx], 
-                                            free_raw_data=False)
-                lgb_hyper['valid_sets'] = lgb.Dataset(X.iloc[val_idx, :], 
-                                                      y_val = y.iloc[val_idx], 
+                lgb_hyper['train_set'] = lgb.Dataset(X[train_idx, :], 
+                                                     y[train_idx], 
+                                                     free_raw_data=False)
+                lgb_hyper['valid_sets'] = lgb.Dataset(X[val_idx, :], 
+                                                      y[val_idx], 
                                                       reference=lgb_hyper['train_set'], 
                                                       free_raw_data=False)
+
+                lgb_m = lgb.train(**lgb_hyper)
+                S_train[val_idx, i] = lgb_m.predict(X[val_idx, :], lgb_m.best_iteration)
+                S_test_i[:, j] = lgb_m.predict(T, lgb_m.best_iteration)
                 
-                
-                lgb = lgb.train(**lgb_hyper)
-                val_score_test[val_idx, i] += lgb.predict(lgb_hyper['valid_sets'], lgb.best_iteration_)
-                Final_score_test[:, i] = lgb.predict(T, lgb.best_iteration_)
-            val_score_test[:, i] /= self.n_splits
-            Final_score_test[:, i] /= self.n_splits
+            #mean_idx = (temp_val_score!=0).sum(1)
+            #mean_idx[mean_idx==0] = 1
+            #val_score_test[:, i] = np.true_divide(temp_val_score.sum(axis=1), mean_idx)
+            S_test[:, i] = S_test_i.mean(axis=1)
             i += 1
         
-        results = cross_val_score(self.stacker, val_score_test[:, i], y, cv=3, scoring='roc_auc')
+        miscellany['lgb_cor']= cor_mat(S_train)
+        train_auc = []
+        train_gini = []
+        for m in range(i):
+            train_auc.append(metrics.roc_auc_score(y,S_train[:,m]))
+            train_gini.append(data_util.eval_gini(y,S_train[:,m]))
+        miscellany['train_auc']  = train_auc
+        miscellany['train_gini'] = train_gini
+        
+        
+        if META == 'lgb':
+
+            X_copy = np.hstack((X, S_train))
+            T_copy = np.hstack((T, S_test))
+            X_train, X_val, y_train, y_val = train_test_split(X_copy, y ,test_size =0.2 ,random_state=rso)
+            lgb_hyper = self.base_models['lgb'][0]
+            lgb_hyper['train_set'] = lgb.Dataset(X_train, 
+                                                     y_train, 
+                                                     free_raw_data=False)
+            lgb_hyper['valid_sets'] = lgb.Dataset(X_val, 
+                                                      y_val, 
+                                                      reference=lgb_hyper['train_set'], 
+                                                      free_raw_data=False)
+            lgb_m1 = lgb.train(**lgb_hyper)
+            
+            y_pred = lgb_m1.predict(X_val, lgb_m1.best_iteration)
+            res = lgb_m1.predict(T, lgb_m.best_iteration)
+            
+            result = metrics.roc_auc_score(y_val,y_pred)
+            gini = data_util.eval_gini(y_val,y_pred)
+
+            print("Stacker score: {}. Gini score: {}.".format(result, gini))
+
+            return res, miscellany
+        
+        if META == 'lg':
+            X_copy = np.hstack((X, val_score_test))
+            T_copy = np.hstack((T, Final_score_test))
+            
+            
+
+        results = cross_val_score(self.stacker, S_train, y, cv=3, scoring='roc_auc')
         print(results)
         print("Stacker score: %.5f" % (results.mean()))
 
-        self.stacker.fit(val_score_test, y)
-        res = self.stacker.predict_proba(Final_score_test)[:,1]
-        return res
+        self.stacker.fit(S_train, y)
+        res = self.stacker.predict_proba(S_test)[:,1]
+        return res, miscellany
 
 
-        
+def cor_mat(x):
+    mean_x = x- np.mean(x, axis=1, keepdims= True)
+    sq_x = np.sum(mean_x**2, axis=0, keepdims = True)
+    cor = np.dot(mean_x.T, mean_x) / np.sqrt(np.dot(sq_x.T, sq_x))
+    return cor
+    
         
 # lgb2_params = {
 #     'boosting_type':'gbdt',
